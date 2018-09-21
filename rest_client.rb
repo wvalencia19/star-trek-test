9c2c61f540cb15f93d6ceed591c3168472741c97
Wilson Valencia <wvalencia@tappsi.co>
2018-09-20 21:32:59 -0300

    Add RestClient class

--- rest_client.rb
+++ rest_client.rb
@@ -1,5 +1,22 @@
-class RestClient
-  def request
+require 'httparty'
 
+class RestClient
+  SERVER_HOST = 'http://stapi.co/api/v1/rest/character'
+  include HTTParty
+  base_uri SERVER_HOST
+
+  def get_character_by_name(name)
+    headers = {
+        'Content-Type' => 'application/x-www-form-urlencoded'
+    }
+    options = {query: {name: name}, headers: headers}
+
+    self.class.post("/search", options)
+  end
+
+  def get_character_by_uid(uid)
+    options = {query: {uid: uid}}
+
+    self.class.get("/", options)
   end
 end
